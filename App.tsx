16b877edb2be46286b71832868c21a041a5be16d
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2025-08-10 17:59:18 -0300

    feat: implement ToastAlert component for success and error feedback

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { Routes } from './src/routes';
+import Toast from 'react-native-toast-message';
 
 export default function App() {
 	return (
 		<NavigationContainer>
 			<Routes />
+			<Toast />
 		</NavigationContainer>
 	);
 }
